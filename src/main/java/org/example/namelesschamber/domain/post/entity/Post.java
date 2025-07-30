9287f5e9540e74c37b5e5c40f49654d33c7f9aef
respectwo2 <respectwo@gmail.com>
2025-07-30 17:38:33 +0900

    feat: Post 도메인 초기 설계

---
+++ src/main/java/org/example/namelesschamber/domain/post/entity/Post.java
@@ -0,0 +1,30 @@
+package org.example.namelesschamber.domain.post.entity;
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "posts")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Post {
+
+    @Id
+    private String id;
+
+    private String content;
+    //비회원 사용자 식별용 UUID
+    private String anonymousToken;
+    //멤버 식별용 ID
+    private String userId;
+
+    private boolean isDeleted;
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+}
