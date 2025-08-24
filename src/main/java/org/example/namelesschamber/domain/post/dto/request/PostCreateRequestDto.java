0a2717f6f41b82def94712f1e62ca1aee3c0be6b
respectwo2 <respectwo@gmail.com>
2025-08-24 11:27:28 +0900

    refactor: 작성 길이 예외처리

--- src/main/java/org/example/namelesschamber/domain/post/dto/request/PostCreateRequestDto.java
+++ src/main/java/org/example/namelesschamber/domain/post/dto/request/PostCreateRequestDto.java
@@ -2,12 +2,14 @@ package org.example.namelesschamber.domain.post.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.example.namelesschamber.domain.post.entity.PostType;
 
 public record PostCreateRequestDto(
         @NotBlank
         String title,
         @NotBlank
+        @Size(min = 100, message = "조금 더 이야기해주세요. 100자 이상 적어야 흘려보낼 수 있어요.")
         String content,
         @NotNull
         PostType type
