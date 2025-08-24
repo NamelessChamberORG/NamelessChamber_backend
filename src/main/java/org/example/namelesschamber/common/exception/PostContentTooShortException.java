3d83779ddb0dc404c482dc0d14dcc20405fa5248
respectwo2 <respectwo@gmail.com>
2025-08-24 11:30:50 +0900

    refactor: 작성 길이 예외처리

--- src/main/java/org/example/namelesschamber/common/exception/PostContentTooShortException.java
+++ src/main/java/org/example/namelesschamber/common/exception/PostContentTooShortException.java
@@ -3,7 +3,7 @@ package org.example.namelesschamber.common.exception;
 public class PostContentTooShortException extends CustomException {
 
     public PostContentTooShortException(){
-        super(ErrorCode.POST_NOT_TOO_SHORT);
+        super(ErrorCode.POST_CONTENT_TOO_SHORT);
 
     }
 }
