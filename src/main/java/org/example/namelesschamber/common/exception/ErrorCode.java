470d896e0c0e0cc0a98fe2263dab75f9c644cb44
respectwo2 <respectwo@gmail.com>
2025-09-02 11:01:08 +0900

    refactor: 응답 포맷 변경 작업

--- src/main/java/org/example/namelesschamber/common/exception/ErrorCode.java
+++ src/main/java/org/example/namelesschamber/common/exception/ErrorCode.java
@@ -12,10 +12,10 @@ public enum ErrorCode {
     POST_NOT_FOUND(HttpStatus.NOT_FOUND,1002, "해당 ID의 게시글을 찾을 수 없습니다."),
     POST_ALREADY_DELETED(HttpStatus.BAD_REQUEST,1003,  "이미 삭제된 게시글입니다."),
     DUPLICATE_NICKNAME(HttpStatus.BAD_REQUEST,1004, "이미 사용 중인 닉네임입니다."),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND,1006, "존재하지 않는 사용자입니다."),
-    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED,1007, "비밀번호가 올바르지 않습니다."),
-    CONTENT_TOO_SHORT_FOR_SHORT(HttpStatus.BAD_REQUEST,1008, "조금 더 이야기해주세요. 30자 이상 적어야 흘려보낼 수 있어요."),
-    CONTENT_TOO_SHORT_FOR_LONG(HttpStatus.BAD_REQUEST,1009, "조금 더 이야기해주세요. 100자 이상 적어야 흘려보낼 수 있어요.");
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND,1005, "존재하지 않는 사용자입니다."),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED,1006, "비밀번호가 올바르지 않습니다."),
+    CONTENT_TOO_SHORT_FOR_SHORT(HttpStatus.BAD_REQUEST,1007, "조금 더 이야기해주세요. 30자 이상 적어야 흘려보낼 수 있어요."),
+    CONTENT_TOO_SHORT_FOR_LONG(HttpStatus.BAD_REQUEST,1008, "조금 더 이야기해주세요. 100자 이상 적어야 흘려보낼 수 있어요.");
 
     private final HttpStatus status;
     private final int code;
