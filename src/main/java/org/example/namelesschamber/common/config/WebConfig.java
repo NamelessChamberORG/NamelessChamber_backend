01987a41d81fa9c80cea45328710dc9fa00529a8
respectwo2 <respectwo@gmail.com>
2025-09-05 00:16:54 +0900

    fix: preflight 오픈

--- src/main/java/org/example/namelesschamber/common/config/WebConfig.java
+++ src/main/java/org/example/namelesschamber/common/config/WebConfig.java
@@ -1,16 +1,18 @@
 package org.example.namelesschamber.common.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableConfigurationProperties(CorsProperties.class)
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig {
-
-    @Value("${app.cors.allowed-origins}")
-    private String[] allowedOrigins;
+    private final CorsProperties corsProperties;
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -18,9 +20,9 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(allowedOrigins)
+                        .allowedOrigins(corsProperties.allowedOrigins().toArray(new String[0]))
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("Content-Type", "Authorization")
+                        .allowedHeaders("*")
                         .allowCredentials(true);
             }
         };
