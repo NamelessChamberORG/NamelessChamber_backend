debb1df5a1a4f938c4d9be8abeab706d1284f505
respectwo2 <respectwo@gmail.com>
2025-09-04 22:48:27 +0900

    refactor: signup & login 이메일로 변경

--- src/main/java/org/example/namelesschamber/common/exception/ErrorCode.java
+++ src/main/java/org/example/namelesschamber/common/exception/ErrorCode.java
@@ -16,7 +16,7 @@ public enum ErrorCode {
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED,1006, "비밀번호가 올바르지 않습니다."),
     CONTENT_TOO_SHORT_FOR_SHORT(HttpStatus.BAD_REQUEST,1007, "조금 더 이야기해주세요. 30자 이상 적어야 흘려보낼 수 있어요."),
     CONTENT_TOO_SHORT_FOR_LONG(HttpStatus.BAD_REQUEST,1008, "조금 더 이야기해주세요. 100자 이상 적어야 흘려보낼 수 있어요."),
-    DUPLICATE_EMAIL(HttpStatus.BAD_REQUEST,1004, "이미 사용 중인 닉네임입니다.");
+    DUPLICATE_EMAIL(HttpStatus.BAD_REQUEST,1009, "이미 사용 중인 이메일입니다.");
 
     private final HttpStatus status;
     private final int code;
