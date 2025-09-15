ef66ff1f91f8794974275a68a4641cf7fd86a19e
respectwo2 <respectwo@gmail.com>
2025-09-15 16:09:39 +0900

    feat: 리프레시토큰 구현 및 로그아웃 API 개발

--- src/main/java/org/example/namelesschamber/common/security/JwtAuthenticationFilter.java
+++ src/main/java/org/example/namelesschamber/common/security/JwtAuthenticationFilter.java
@@ -1,10 +1,10 @@
 package org.example.namelesschamber.common.security;
 
-import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -23,9 +23,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtTokenProvider jwtTokenProvider;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+
         String token = resolveToken(request);
         log.debug("Incoming token: {}", token);
 
