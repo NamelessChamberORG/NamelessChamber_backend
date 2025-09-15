63a342b3ea3bfd4d3c29e4fa698edd844dbe1fdd
respectwo2 <respectwo@gmail.com>
2025-09-15 22:58:31 +0900

    feat: 스프링 시큐리티 예외 처리

--- src/main/java/org/example/namelesschamber/common/config/SecurityConfig.java
+++ src/main/java/org/example/namelesschamber/common/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.example.namelesschamber.common.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
